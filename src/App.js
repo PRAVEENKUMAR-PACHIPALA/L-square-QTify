1da7942706001aed162c747bb9ef6c6193844fcc
PRAVEENKUMAR-PACHIPALA <81503281+PRAVEENKUMAR-PACHIPALA@users.noreply.github.com>
2023-11-28 11:00:07 +0530

    Module 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Navbar from "./components/Navbar/Navbar"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
     </>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "../../components/Navbar/Navbar";
+import HeroSection from "../../components/Hero/Hero";
+
+//Accordion Data.(Sending as a props).
+
+function Home() {
+
+	return (
+		<>
+			<NavBar />
+			<HeroSection />
+		</>
+	);
+}
+
+export default Home;
