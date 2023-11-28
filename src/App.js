40c8aa137a4d1dadac15c20e0bdc54e0dc93c46c
PRAVEENKUMAR-PACHIPALA <81503281+PRAVEENKUMAR-PACHIPALA@users.noreply.github.com>
2023-11-28 23:35:01 +0530

    Module 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import React, {useState, useEffect} from 'react';
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import { StyledEngineProvider } from '@mui/material';
+import { fetchTopAlbums } from './api/api';
+import { fetchNewAlbums } from './api/api';
 
 function App() {
 
+  const [data, setData] = useState({});
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        return {...prevState, [key]: data};
+      })
+    })
+  }
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+  }, [])
+
+  const { topAlbums=[], newAlbums=[] } = data;
+
   return (
     <>
-      <Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
+      <StyledEngineProvider injectFirst>
+        <Navbar />
+        <Outlet context={{data: {topAlbums, newAlbums}}} />
+      </StyledEngineProvider>
     </>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import Section from "../../components/Section/Section";
+import { useOutletContext } from "react-router-dom";
+import {styles} from "./HomePage.module.css"
+
+function Home(props) {
+
+	const data = useOutletContext();
+	const { topAlbums, newAlbums } = data;
+
+	return (
+		<>
+			<Hero />
+			<div className={StyleSheet.wrapper}>
+				<Section title="Top Albums" data={topAlbums} type="album" />
+				<Section title="New Albums" data={newAlbums} type="album" />
+			</div>
+		</>
+	);
+}
+
+export default Home;
